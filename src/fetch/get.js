f27fcad121842f4d84dbf5412ea9ccafbd489210
colaboy <49201650@qq.com>
2017-07-28 14:49:02 +0800

    data数据

--- src/fetch/get.js
+++ src/fetch/get.js
@@ -1,13 +1,13 @@
 import 'whatwg-fetch'
 import 'es6-promise'
 
-export function get(url) {
+export function get (url) {
   var result = fetch(url, {
-      credentials: 'include',
-      headers: {
-          'Accept': 'application/json, text/plain, */*'
-      }
-  });
+    credentials: 'include',
+    headers: {
+      'Accept': 'application/json, text/plain, */*'
+    }
+  })
 
-  return result;
+  return result
 }
