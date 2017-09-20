df95906c712b419a89faf0443ab64f9594ee809a
wangmingzhu <wangmingzhu@waiqin365.com>
2017-09-20 09:24:28 +0800

    'main.js'

--- src/main.js
+++ src/main.js
@@ -14,7 +14,13 @@ Vue.prototype.$http = http
 // 配置全局组件
 import seedsui from '@/components/seedsui/seedsui'
 Vue.use(seedsui)
-
+// 配置快速点击
+import FastClick from 'fastclick'
+if ('addEventListener' in document) {
+  document.addEventListener('DOMContentLoaded', () => {
+    FastClick.attach(document.body)
+  }, false)
+}
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
